0c22e70e65e6b901b8b3d3ad2d5369b7c2dd1a73
Lavrov-Alexey <lavrov-box@mail.ru>
2021-05-17 12:47:50 +0300

    Практическое задание Лаврова Алексея к уроку 4 "Основы Python" на Geek Brains

---
+++ task_4_3.py
@@ -0,0 +1,54 @@
+"""Урок 4. Знакомство с Python
+3. *(вместо 2) Доработать функцию currency_rates(): теперь она должна возвращать
+кроме курса дату, которая передаётся в ответе сервера. Дата должна быть в виде
+объекта date. Подумайте, как извлечь дату из ответа, какой тип данных лучше
+использовать в ответе функции?"""
+
+import requests
+from decimal import Decimal
+from datetime import datetime
+
+
+def currency_rates(code_currency):
+    """ Returns actual currency rate in RUR for his code (eg "USD") and date of rate
+    (in datetime.date format) from source: http://www.cbr.ru/scripts/XML_daily.asp
+    If code currency is not find => None"""
+
+    # делаем нечувствительность к регистру переданного кода валюты
+    code_currency = code_currency.upper()
+    response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
+
+    # проверяем - есть ли переданный код валюты в ответе ресурса (нет => None)
+    if response.text.find(code_currency) == -1:
+        return None, None  # для совместимости возвращаем тоже tuple
+
+    # вычленяем собственно значение курса запрошенной валюты и дату
+    current_rate = response.text.split(f"<CharCode>{code_currency}</CharCode>")
+    # отвлекаемся на дату
+    date_rate = current_rate[0].split('<ValCurs Date="')
+    date_rate = date_rate[1][:10]  # здесь у нас уже есть дата
+    # преобразуем дату в формат datetime
+    date_rate = datetime.strptime(date_rate, '%d.%m.%Y')
+    # занимаемся дальше курсом
+    current_rate = current_rate[1].split('<Value>')
+    current_rate = current_rate[1].split('</Value>')[0]
+
+    # для перевода курса в числовой тип меняем разделитель дробной части
+    current_rate = current_rate.replace(",", ".")
+    return Decimal(current_rate), date_rate
+
+
+if __name__ == "__main__":
+    # коды валют для тестов
+    TEST_CODE_CURRENCY = "USD", "EUR", "FFF", "usd", "eUr"
+
+    # блок вывода
+    print("\nВ качестве источника курсов валют используется ресурс: "
+          "http://www.cbr.ru/scripts/XML_daily.asp\n")
+    for code_curr in TEST_CODE_CURRENCY:
+        my_rate, my_date = currency_rates(code_curr)
+        if my_rate:  # если курс есть (не None)
+            print(f"На {my_date.strftime('%A, %d.%m.%Y')} "
+                  f"курс {code_curr}: {my_rate} руб.")
+        else:
+            print(rf"Код валюты {code_curr}: отсутствует в источнике. ¯\_(ツ)_/¯")
